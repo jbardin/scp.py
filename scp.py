33be4805314b3fc37815ae420ee00bfe12802575
James Bardin <jbardin@litl.com>
2013-03-07 16:26:47 -0500

    Trying to merge pull req 4
    We need to fix the recursive transfer situation, but pull request 4 is
    kind of rough.
    
    Fixed style issues.
    
    Still has algo problems

--- scp.py
+++ scp.py
@@ -8,15 +8,15 @@
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
-# 
+#
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 
 
 """
@@ -27,21 +27,23 @@ import os
 import fnmatch
 from socket import timeout as SocketTimeout
 
+
 class SCPClient(object):
     """
     An scp1 implementation, compatible with openssh scp.
     Raises SCPException for all transport related errors. Local filesystem
-    and OS errors pass through. 
+    and OS errors pass through.
 
-    Main public methods are .put and .get 
-    The get method is controlled by the remote scp instance, and behaves 
+    Main public methods are .put and .get
+    The get method is controlled by the remote scp instance, and behaves
     accordingly. This means that symlinks are resolved, and the transfer is
     halted after too many levels of symlinks are detected.
     The put method uses os.walk for recursion, and sends files accordingly.
     Since scp doesn't support symlinks, we send file symlinks as the file
     (matching scp behaviour), but we make no attempt at symlinked directories.
     """
-    def __init__(self, transport, buff_size = 16384, socket_timeout = 5.0, progress = None):
+    def __init__(self, transport, buff_size=16384, socket_timeout=5.0,
+                 progress=None):
         """
         Create an scp1 client.
 
@@ -51,7 +53,8 @@ class SCPClient(object):
         @type buff_size: int
         @param socket_timeout: channel socket timeout in seconds
         @type socket_timeout: float
-        @param progress: callback - called with (filename, size, sent) during transfers
+        @param progress: callback - called with (filename, size, sent) during
+            transfers
         @type progress: function(string, int, int)
         """
         self.transport = transport
@@ -63,14 +66,9 @@ class SCPClient(object):
         self._recv_dir = ''
         self._utime = None
         self._dirtimes = {}
-        
-    def __del__(self):
-        if self.channel:
-            self.channel.close()
-        
 
-    def put(self, files, remote_path = '.', 
-            recursive = False, preserve_times = False):
+    def put(self, files, remote_path='.',
+            recursive=False, preserve_times=False):
         """
         Transfer files to remote host.
 
@@ -92,20 +90,21 @@ class SCPClient(object):
         scp_command = ('scp -t %s', 'scp -r -t %s')[recursive]
         self.channel.exec_command(scp_command % remote_path)
         self._recv_confirm()
-       
+
         if not isinstance(files, (list, tuple)):
             files = [files]
-        
-        self._send_files(files,recursive=recursive)
-        
-    
-    def get(self, remote_path, local_path = '',
-            recursive = False, preserve_times = False):
+
+        self._send_files(files, recursive=recursive)
+        if self.channel:
+            self.channel.close()
+
+    def get(self, remote_path, local_path='',
+            recursive=False, preserve_times=False):
         """
         Transfer files from remote host to localhost
 
         @param remote_path: path to retreive from remote host. since this is
-            evaluated by scp on the remote host, shell wildcards and 
+            evaluated by scp on the remote host, shell wildcards and
             environment variables may be used.
         @type remote_path: str
         @param local_path: path in which to receive files locally
@@ -116,14 +115,15 @@ class SCPClient(object):
             and directories.
         @type preserve_times: bool
         """
-        self._recv_dir = local_path or os.getcwd() 
+        self._recv_dir = local_path or os.getcwd()
         rcsv = ('', ' -r')[recursive]
         prsv = ('', ' -p')[preserve_times]
         self.channel = self.transport.open_session()
         self.channel.settimeout(self.socket_timeout)
         self.channel.exec_command('scp%s%s -f %s' % (rcsv, prsv, remote_path))
         self._recv_all()
-
+        if self.channel:
+            self.channel.close()
 
     def _read_stats(self, name):
         """return just the file stats needed for scp"""
@@ -133,115 +133,127 @@ class SCPClient(object):
         atime = int(stats.st_atime)
         mtime = int(stats.st_mtime)
         return (mode, size, mtime, atime)
-    
-    def _send_single_files(self,files):
-        if isinstance(files,str):files=[files]
-        for name in files:
-           basename = os.path.basename(name)
-           (mode, size, mtime, atime) = self._read_stats(name)
-           if self.preserve_times:
-               self._send_time(mtime, atime)
-           file_hdl = file(name, 'rb')
-           self.channel.sendall('C%s %d %s\n' % (mode, size, basename))
-           self._recv_confirm()
-           file_pos = 0
-           buff_size = self.buff_size
-           chan = self.channel
-           if self._progress:
-               self._progress(name, size, 0)        #send 0 progress to detect begin :)
-           while file_pos < size:
-               chan.sendall(file_hdl.read(buff_size))
-               file_pos = file_hdl.tell()
-               if self._progress:
-                   self._progress(name, size, file_pos)
-           chan.sendall('\x00')
-           file_hdl.close()
-        
 
-    def _send_files(self, files,filter=[],recursive=False): 
+    def _send_single_files(self, files):
+        if isinstance(files, basestring):
+            files = [files]
+        for name in files:
+            basename = os.path.basename(name)
+            (mode, size, mtime, atime) = self._read_stats(name)
+            if self.preserve_times:
+                self._send_time(mtime, atime)
+            file_hdl = file(name, 'rb')
+            self.channel.sendall('C%s %d %s\n' % (mode, size, basename))
+            self._recv_confirm()
+            file_pos = 0
+            buff_size = self.buff_size
+            chan = self.channel
+            if self._progress:
+                # send 0 progress to detect begin :)
+                self._progress(name, size, 0)
+            while file_pos < size:
+                chan.sendall(file_hdl.read(buff_size))
+                file_pos = file_hdl.tell()
+                if self._progress:
+                    self._progress(name, size, file_pos)
+            chan.sendall('\x00')
+            self._recv_confirm()
+            file_hdl.close()
+
+    def _send_files(self, files, filter=[], recursive=False):
         """
         Send files to remote destination
-            
-        @param files: list of files or paths to be transferred to remote destination (all files are put into the dest. dir!)
+
+        @param files: list of files or paths to be transferred to remote
+            destination (all files are put into the dest. dir!)
         @type files: either str or list
-        @param filter: list of fnmatch filters (*,*.c,..) empty list = no filtering
+        @param filter: list of fnmatch filters (*,*.c,..) empty list = no
+            filtering
         @type filter: either str or list
         @param recursive: recurse into subdirs
         @type recursive: boolean
         """
         # for loop for recursive transfers
         # will only return one dir if recursive = False
-        #handle multiple paths
-        if isinstance(files,str):files=[files]
+        # handle multiple paths
         for base in files:
             if os.path.isfile(base):
-                #is file
-                self._send_single_files(base)               
+                # is file
+                self._send_single_files(base)
             elif os.path.isdir(base):
-                #is pure dir
-                last=base   #save base for get_dir_level_diff
-                for root, fls in self._list_dir(base,filter,recursive=recursive):
+                # is pure dir
+                last = base   # save base for get_dir_level_diff
+                for root, fls in self._list_dir(base, filter,
+                                                recursive=recursive):
                     # build file-list for transfer, and transfer file by file
-                    pop,push= self.__traverse_dir_instruction(last,root)
+                    pop, push = self._traverse_dir_instruction(last, root)
                     last = root
                     for i in range(pop):
                         self._send_popd()
                     for i in range(push):
                         self._send_pushd(root)
-                    
-                    #actually transfer files
-                    self._send_single_files([os.path.join(root, f) for f in fls])
-            elif not os.path.isfile(base) and not os.path.isdir(base):
-                #wildcarded filename               
-                for root,fls in self._list_dir(os.path.dirname(base),filters=os.path.basename(base),recursive=False):
-                    self._send_single_files([os.path.join(root, f) for f in fls])
-                
-                
 
-              
-    def _list_dir(self,paths,filters=[],recursive=False):
-        ffiles=[]
-        if isinstance(filters,str):filters=[filters]  
-        if isinstance(paths,str):paths=[paths]    
-        
+                    # actually transfer files
+                    self._send_single_files([os.path.join(root, f)
+                                            for f in fls])
+            elif not os.path.isfile(base) and not os.path.isdir(base):
+                # wildcarded filename
+                for root, fls in self._list_dir(os.path.dirname(base),
+                                                filters=os.path.basename(base),
+                                                recursive=False):
+                    self._send_single_files([os.path.join(root, f)
+                                            for f in fls])
+
+    def _list_dir(self, paths, filters=[], recursive=False):
+        ffiles = []
+        if isinstance(filters, basestring):
+            filters = [filters]
+        if isinstance(paths, basestring):
+            paths = [paths]
+
         for root in paths:
-            for root,dirs,files in os.walk(root,topdown=True):
-                if len(filters)>0:
-                    ffiles=[]
+            for root, dirs, files in os.walk(root, topdown=True):
+                if len(filters) > 0:
+                    ffiles = []
                     for f in filters:
-                        ffiles+= fnmatch.filter(files,f)   #join with prev. files  
-                    #return root dir + unique files
-                    files=ffiles    #filtered files
-                yield root,set(files) 
-                if not recursive: break 
-             
-             
-    def __normalize_path(self,p):
-        p = os.path.normpath(p) # normalize /x/../A/./../b/ to /b/
-        p=p.replace("\\","/")   # normalize path to unix path
-        return p 
-                  
-    def __traverse_dir_instruction(self,a,b):
+                        # join with prev. files
+                        ffiles += fnmatch.filter(files, f)
+                    # return root dir + unique files
+                    files = ffiles    # filtered files
+                yield root, set(files)
+                if not recursive:
+                    break
+
+    def _normalize_path(self, p):
+        p = os.path.normpath(p)  # normalize /x/../A/./../b/ to /b/
+        p = p.replace("\\", "/")  # normalize path to unix path
+        return p
+
+    def _traverse_dir_instruction(self, a, b):
         '''
         Calculate the number of POPs and PUSHs to get from dir a to dir b
-        Returns (pops,pushs)
+        Returns (pops, pushs)
         '''
-        a=self.__normalize_path(a)
-        b=self.__normalize_path(b)
-        if a[-1]=="/": a=a[:-1] # kill trailing / (to avoid extra pushes/pops)
-        if b[-1]=="/": b=b[:-1]
-        last = a.split("/")      # listify
+        a = self._normalize_path(a)
+        b = self._normalize_path(b)
+        if a[-1] == "/":
+             # kill trailing / (to avoid extra pushes/pops)
+            a = a[:-1]
+        if b[-1] == "/":
+            b = b[:-1]
+        last = a.split("/")
         current = b.split("/")
         #compare last to current
-        pos=0
+        pos = 0
         for e in last:
-            if pos>=len(current) or e!=current[pos]: break
-            pos +=1 
+            if pos >= len(current) or e != current[pos]:
+                break
+            pos += 1
         #calc_pops / pushes from last_dir to current dir
-        num_pops= len(last)-pos
-        num_pushs= len(current)-pos
-        return num_pops,num_pushs
-        
+        num_pops = len(last)-pos
+        num_pushs = len(current)-pos
+        return num_pops, num_pushs
+
     def _send_pushd(self, directory):
         (mode, size, mtime, atime) = self._read_stats(directory)
         basename = os.path.basename(directory)
@@ -276,7 +288,7 @@ class SCPClient(object):
             raise SCPException('No response from server')
         else:
             raise SCPException('Invalid response from server: ' + msg)
-    
+
     def _recv_all(self):
         # loop over scp commands, and recive as necessary
         command = {'C': self._recv_file,
@@ -287,7 +299,8 @@ class SCPClient(object):
             # wait for command as long as we're open
             self.channel.sendall('\x00')
             msg = self.channel.recv(1024)
-            if not msg: # chan closed while recving
+            if not msg:
+                # chan closed while recving
                 break
             code = msg[0]
             try:
@@ -296,7 +309,7 @@ class SCPClient(object):
                 raise SCPException(repr(msg))
         # directory times can't be set until we're done writing files
         self._set_dirtimes()
-    
+
     def _set_time(self, cmd):
         try:
             times = cmd.split()
@@ -319,7 +332,7 @@ class SCPClient(object):
             chan.send('\x01')
             chan.close()
             raise SCPException('Bad file format')
-        
+
         try:
             file_hdl = file(path, 'wb')
         except IOError, e:
@@ -339,7 +352,7 @@ class SCPClient(object):
                 pos = file_hdl.tell()
                 if self._progress:
                     self._progress(path, size, pos)
-            
+
             msg = chan.recv(512)
             if msg and msg[0] != '\x00':
                 raise SCPException(msg[1:])
@@ -381,7 +394,7 @@ class SCPClient(object):
 
     def _recv_popd(self, *cmd):
         self._recv_dir = os.path.split(self._recv_dir)[0]
-        
+
     def _set_dirtimes(self):
         try:
             for d in self._dirtimes:
