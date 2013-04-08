c4dd22a81e35025160d141f9b76d02be86d0adb4
James Bardin <jbardin@litl.com>
2013-04-08 11:00:18 -0400

    Add description

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ from setuptools import setup
 
 setup(
         name = 'scp',
-        version = '0.5',
+        version = '0.5.1',
         author = 'James Bardin',
         author_email = 'j.bardin@gmail.com',
         license = 'LGPL',
         url = 'https://github.com/jbardin/scp.py.git',
+        description='scp module for paramiko',
         py_modules = ['scp'],
         install_requires = ['paramiko'],
 )
